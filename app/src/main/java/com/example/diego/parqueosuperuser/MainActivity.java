18624476ccea4dab264eb938636bdceb2b8c169d
Diego <diegowrhasta@gmail.com>
2018-10-03 19:54:11 -0400

    Menu Principal, nombres de funciones, y actividades respectivas

--- app/src/main/java/com/example/diego/parqueosuperuser/MainActivity.java
+++ app/src/main/java/com/example/diego/parqueosuperuser/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.diego.parqueosuperuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registro_encargado(View view) {
+    }
+
+    public void inicio_reportes(View view) {
+    }
+
+    public void inicio_calles(View view) {
+    }
+
+    public void lista_encargados(View view) {
     }
 }
